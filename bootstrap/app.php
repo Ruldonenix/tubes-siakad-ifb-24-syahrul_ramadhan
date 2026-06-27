0410b90237524b90f7f467b7cf1a2fc5f7e9fdba
Syahrul Ramadhan <syahruls90807512@gmail.com>
2026-06-27 20:48:29 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(DIR))
     ->withRouting(
-        web: DIR.'/../routes/web.php',
-        commands: DIR.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
